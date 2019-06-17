b33bb6f466d7ee767132e8e28028d0ee670deeaf
Anastasiia <anastasiiadev@gmail.com>
2019-06-17 22:47:52 +0700

    Added connection to the db and implemented edit of the type of the check

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ var mysql      = require( 'mysql' );
 var connection = mysql.createConnection( {
     "host"     : "127.0.0.1",
     "user"     : "root",
-    "password" : "Banana4093",
+    "password" : "",
     "database" : "course-project",
     "dialect"  : "mysql"
 } );
